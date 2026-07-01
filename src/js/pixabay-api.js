f86b0775bbb35d5226e2833f852957711ab04a28
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-07-01 16:11:54 +0300

    Preparation

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '56510220-94bf2d241d837d21bc9d081cd';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
